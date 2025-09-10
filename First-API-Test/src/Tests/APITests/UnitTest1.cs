80b0566a69a0f0d87dd8e8b4fe66086d493ac07f
SalavatYuldashev <salavat.yuldashev@vk.com>
2025-09-10 17:37:08 +0300

    base project structure

---
+++ First-API-Test/src/Tests/APITests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace First_API_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
